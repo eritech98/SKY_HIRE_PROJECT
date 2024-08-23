8fa252bc771bb5cd8f66025e75a6c5ac1793070a
Tsakani Mohale <tsakanimohale4@gmail.com>
2024-08-24 01:15:55 +0300

    Updated main.js file

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
-// JavaScript to handle form submission
-        document.getElementById('uniqueFormId').addEventListener('submit', function(event) {
-            event.preventDefault(); // Prevent the form from submitting normally
+// This is the main custmized javascript  which will handle form submission
+// However the form was removed  from the main project and its not necessary
+// 
+     /*document.getElementById('uniqueFormId').addEventListener('submit', function(event) {
+           event.preventDefault(); // Prevent the form from submitting normally
             // Hide the form
             document.getElementById('uniqueFormId').style.display = 'none';
             // Show the modal
             $('#thankYouModalId').modal('show');
-        });
+        });*/
